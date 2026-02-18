6a503d057895789bd1d2a06b6df8d1b80ac6ec97
withjoono <75061161+withjoono@users.noreply.github.com>
2026-02-19 05:06:49 +0900

    feat: add auto-login loading overlay to SSOListener

--- backend/dist/app.module.js
+++ backend/dist/app.module.js
@@ -8,16 +8,27 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
+const config_1 = require("@nestjs/config");
+const typeorm_1 = require("@nestjs/typeorm");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
+const typeorm_config_service_1 = require("./typeorm-config.service");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
+        imports: [
+            config_1.ConfigModule.forRoot({
+                isGlobal: true,
+                envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+            }),
+            typeorm_1.TypeOrmModule.forRootAsync({
+                useClass: typeorm_config_service_1.TypeOrmConfigService,
+            }),
+        ],
         controllers: [app_controller_1.AppController],
-        providers: [app_service_1.AppService],
+        providers: [app_service_1.AppService, typeorm_config_service_1.TypeOrmConfigService],
     })
 ], AppModule);
 //# sourceMappingURL=app.module.js.map
